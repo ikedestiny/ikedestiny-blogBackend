a8c7826744ae5fd714d23477f1f4805ae2c5e781
Ike Holy Destiny <121898018+ikedestiny@users.noreply.github.com>
2025-01-10 19:45:18 +0300

    cors

--- src/main/java/com/example/blogbackend/controller/BlogPostController.java
+++ src/main/java/com/example/blogbackend/controller/BlogPostController.java
@@ -16,21 +16,25 @@ public class BlogPostController {
     SearchRepo searchRepo;
 
     @GetMapping("/posts")
+    @CrossOrigin(origins = "*")
     public List<BlogPost> getAll(){
         return repo.findAll();
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/create-new-blog")
     public BlogPost createNewBlog(@RequestBody BlogPost blogPost){
         return repo.save(blogPost);
     }
 
+    @CrossOrigin(origins = "*")
     @DeleteMapping("/delete/{id}")
     public String deleteById(@PathVariable String id){
         repo.deleteById(id);
         return "successfully deleted post with id "+id;
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/search/{text}")
     public List<BlogPost> search(@PathVariable String text){
         return searchRepo.fullTextSearch(text);
